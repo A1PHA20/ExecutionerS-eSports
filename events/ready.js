745f36b93008274bb6496dea331ec4c907aaa933
A1PHA <88451802+A1PHA20@users.noreply.github.com>
2022-01-08 13:17:12 +0600

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 const client = require('../index')
 client.on("ready", () => {
     console.log(`${client.user.tag} is Online!`)
-    client.user.setActivity(`How RedZone Coded ME!`, {
+    client.user.setActivity(`Mia Khalifa's Porn`, {
         type: "WATCHING"
     })
     client.user.setStatus("dnd")
